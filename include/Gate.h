19cf2ff164dfed2536fc06a59277ece4f55e806d
Logrus <idlogrus@gmail.com>
2017-04-22 18:31:48 +0200

    Define move ctor.

--- include/Gate.h
+++ include/Gate.h
@@ -11,9 +11,25 @@
 class Gate
 {
 public:
-    typedef std::unique_ptr<Gate> Ptr;
+    typedef std::shared_ptr<Gate> Ptr;
 
-    inline Gate(bool u) : unary(u) {}
+    Gate(bool u) : unary(u) {}
+    Gate(const Gate& g) = delete;
+    Gate(Gate&& g) noexcept : 
+        unary(g.unary), 
+        output(g.output),
+        gradout_a(g.gradout_a),
+        gradout_b(g.gradout_b) {
+            input_a = g.input_a;
+            g.input_a = nullptr;
+            input_b = g.input_b;
+            g.input_b = nullptr;
+            grad_input = g.grad_input;
+            g.grad_input = nullptr;
+        }
+    
+    Gate& operator=(const Gate& ) = delete;
+    Gate& operator=(Gate&& g) = delete;
 
     virtual void forward() = 0;
     virtual void backward() = 0;
